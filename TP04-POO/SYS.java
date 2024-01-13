71fb03321c9805c3a6a77a383287dd41ecfd3b07
hunter666hz <hzhunter7@gmail.com>
2024-01-13 17:55:05 +0100

    modified:   TP04-POO/SYS.java 	modified:   TP04-POO/StuSys.java

--- TP04-POO/SYS.java
+++ TP04-POO/SYS.java
@@ -1,9 +1,53 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class SYS {
+    static ArrayList<StuSys> students = new ArrayList<StuSys>();
     public static void main(String[] args) {
-        ArrayList<StuSys> students = new ArrayList<StuSys>();
-        StuSys s = new StuSys(0, "", "", "", "", "", ' ');
-        students.add(s );
+        StuSys s = new StuSys(19, "oyu", "cef", "ytes", "you@math", "m");
+        students.add(s);
+         s = new StuSys(0, "oyugbg", "cebgbgf", "ytes", "you@chim", "f");
+         students.add(s);
+          s = new StuSys(19, "oyfvfvf", "cvfvfef", "ytes", "you@phys", "m");
+         students.add(s);
+         System.out.println(students);
+         System.out.println(getEtudiantsMedian());
+         System.out.println(getEtudiantParNom("oyugbg"));
+         System.out.println(getDepartement("oyfvfvf"));
+        System.out.println(getEtudiantParAgeEtSexe(19, "m"));
+    }
+
+    public static HashMap<String, StuSys> getEtudiantsMedian() {
+        HashMap<String, StuSys> median = new HashMap<String, StuSys>();
+        for (StuSys i : students) {
+            if (i.getAge() >= 18 && i.getAge() <= 25) {
+                median.put(i.getName(), i);
+            }
+        }
+        return median;
+    }
+
+    public static StuSys getEtudiantParNom(String name) {
+        for (StuSys stu : students) {
+            if (stu.getName() == name) {
+                return stu;
+            }
+        }
+        return null;
+    }
+
+    public static String getDepartement(String name) {
+        return getEtudiantParNom(name).getDep();
+    }
+
+    public static ArrayList<StuSys> getEtudiantParAgeEtSexe(int minAge, String sexe) {
+        ArrayList<StuSys> spcstudent = new ArrayList<StuSys>();
+        for (StuSys i : students) {
+            if (i.getAge() == minAge && i.getSex() == sexe) {
+                spcstudent.add(i);
+            }
+        }
+        return spcstudent;
     }
 }
--- TP04-POO/StuSys.java
+++ TP04-POO/StuSys.java
@@ -1,19 +1,52 @@
+import java.util.*;
 public class StuSys {
     private int age;
     private String  Name,firstName, matricule, email,departement;  
-    private char sex;
+    private String sex;
+    HashMap<String, String> domanddep = new HashMap<String, String>() {
+        {
+            put("info", "informatiq");
+            put("chim", "chimic");
+            put("math", "nathi");
+            put("phys", "physics");
 
-    public StuSys(int age, String Name, String firstName, String matricule, String email, String departement,
-            char sex) {
+        }
+    };
+
+    public StuSys(int age, String Name, String firstName, String matricule, String email,
+            String sex) {
         this.age = age;
         this.Name = Name;
         this.firstName = firstName;
         this.matricule = matricule;
         this.email = email;
-        this.departement = departement;
+        
         this.sex = sex;
+        getDep(email);
 
 
     }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getName()
+    {
+        return this.Name;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public String getDep() {
+        return this.departement;
+    }
     
+    private void getDep(String email) {
+        String[] x = email.split("@");
+        String a = x[1];
+        departement = domanddep.get(a);
+    }
 }
