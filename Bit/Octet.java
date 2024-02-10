2632d05d3e8c2d3473932ce6c761d21177cc87ec
hunter666hz <hzhunter7@gmail.com>
2024-02-10 21:45:28 +0100

    modified:   Bit/Octet.java

--- Bit/Octet.java
+++ Bit/Octet.java
@@ -1,12 +1,17 @@
 public class Octet {
     private String[] data;
-
+    private Bit[] z ;
     public void Octet(String[] data) throws InvBit {
-        for (int i = 0; i < data.length;i++) {
+        for (int i = 0; i < data.length; i++) {
             if (!data.equals("1") || !data[i].equals("0")) {
                 throw new InvBit();
             }
         }
         this.data = data;
     }
+
+    public String toString() {
+        return this.data.toString();
+    }
+   
 }
